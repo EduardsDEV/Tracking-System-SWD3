22f8457c6ffb44e5d3b9242794fbb8cb7d8c2462
Epharas <cbetivu@gmail.com>
2017-11-15 15:45:11 +0100

    Fix bug with AddInvoiceActivity not having back arrow Make Save button in AddInvoiceActivity to request the web service

--- app/src/main/java/com/das/trackingsystem/AddInvoiceActivity.java
+++ app/src/main/java/com/das/trackingsystem/AddInvoiceActivity.java
@@ -6,6 +6,15 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
+
 public class AddInvoiceActivity extends AppCompatActivity {
 
     @Override
@@ -16,8 +25,55 @@ public class AddInvoiceActivity extends AppCompatActivity {
 
     /** Called when the user taps the Save button is clicked*/
     public void saveInvoice(View view) { // view is the View object that was clicked
-        EditText editText = (EditText) findViewById(R.id.invoiceIdText);
-        // check for empty string
-        Log.i("AddInvoiceActivity", "Invoice ID: " + editText.getText().toString());
+        EditText invoiceIdEditText = (EditText) findViewById(R.id.invoiceIdText);
+
+        String uniqueCode = invoiceIdEditText.getText().toString();
+        String product1UniqueCode = ((EditText) findViewById(R.id.product1IdEditText)).getText().toString();
+        String product2UniqueCode = ((EditText) findViewById(R.id.product2IdEditText)).getText().toString();
+        if (Utility.isNotNull(uniqueCode) && Utility.isNotNull(product1UniqueCode) && Utility.isNotNull(product2UniqueCode)) {
+            RequestParams requestParams = new RequestParams("uniqueCode", uniqueCode);
+            requestParams.add("productsUniqueCodes[]", product1UniqueCode);
+            requestParams.add("productsUniqueCodes[]", product2UniqueCode);
+
+            // add info about customer
+            requestParams.add("firstName", "dummyFirstName");
+            requestParams.add("lastName", "dummyLastName");
+            requestParams.add("email", "dummyEmail@gma.com");
+            requestParams.add("address", "saint carolina");
+            requestParams.add("phoneNumber", "50 23 48 91");
+
+            Log.i("AddInvoiceActivity", "Saving Invoice with ID: " + uniqueCode);
+            invokeWebService(requestParams);
+        }
+
+        Log.i("AddInvoiceActivity", "Invoice ID: " + invoiceIdEditText.getText().toString());
+    }
+
+    public void invokeWebService(RequestParams requestParams) {
+        AsyncHttpClient client = new AsyncHttpClient();
+        // host loopback interface in the emulator
+        client.post("http://10.0.2.2:8080/invoice", requestParams, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                try {
+                    String result = new String(responseBody);
+                    JSONObject obj = new JSONObject(result);
+
+                    if (obj.getInt("id") > 0) {
+                        Log.i("AddInvoiceActivity", "Saved Invoice with ID: " + obj.getString("uniqueCode") + " was saved");
+                    } else {
+                        Log.e("AddInvoiceActivity", "smth");
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                String response = new String(responseBody);
+                Log.i("AddInvoiceActivity", statusCode + " " + response);
+            }
+        });
     }
 }
