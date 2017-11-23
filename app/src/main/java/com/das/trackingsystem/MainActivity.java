0fe0ae583e2017a97930398158c45bada1703c83
Epharas <cbetivu@gmail.com>
2017-11-23 15:08:37 +0100

    Make bigger buttons in main activity

--- app/src/main/java/com/das/trackingsystem/MainActivity.java
+++ app/src/main/java/com/das/trackingsystem/MainActivity.java
@@ -1,11 +1,9 @@
 package com.das.trackingsystem;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
